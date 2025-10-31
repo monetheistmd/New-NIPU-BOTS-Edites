66254d1788d39a16c351c71d847f1892d6b5cd65
monetheistmd <moneyheistmd@gmail.com>
2025-10-31 23:17:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-require('dotenv').config();
-
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || '',
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '',
-  PREFIX: process.env.PREFIX || '!',
-  PORT: process.env.PORT || 8000,
-  BOT_NAME: process.env.BOT_NAME || 'MoneyHeistBot'
+  PREFIX: "!",
+  OWNER_NAME: "Money Heist",
+  SESSION_ID: process.env.SESSION_ID || ""
 };
